c287d6190c09a4cb5f7f9e03e73e9d8f76b8eeea
camilapessoa <camilabentespessoa@gmail.com>
2023-12-21 08:25:47 -0300

    aula 1 - vídeo criando rotas

--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 aula 1 . criando o servidor-> inserção do módulo de import export type module
+
+criando rotas -> "Qual o recurso que eu quero acessar? Conunto de endereços http, rotas"
+url ou rota base "/"
+
+instalação nodemon
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 import http from "http";
 
-const PORT = 3000; //porta lógica padrão para comunicação
+const PORT = 3000; //porta lógica padrão para 
+
+const rotas ={
+    "/": "Curso de Node.js",
+    "/livros": "Entrei na rota livros",
+    "/autores": "Entrei na rota autores"
+};
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/plain"});
-    res.end("Curso de Node.js");
+    res.end(rotas[req.url]);
 })
 
 server.listen(PORT, ()=> { console.log("Servidor escutando!")});
