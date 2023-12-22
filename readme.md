95a0509c9555aceba92aadae9145af4a47d76f19
camilapessoa <camilabentespessoa@gmail.com>
2023-12-22 09:55:47 -0300

    aula 2 video buscando e atualizando livros

--- readme.md
+++ readme.md
@@ -12,3 +12,14 @@ instalação express e refatoração de servidor
 vídeo criando a rota livros
 mockar uma pequena base de dados com livros e criação da rota livros;
 Sugestão: inserir atividade para saber mais sobre json
+
+vídeo criando registros com POST
+criação de rota post e utilização do push para adicionar em memória
+
+vídeo buscando e atualizando livros
+O Objeto requisição (estudar)
+params da rota -> estamos passando os parâmetros de busca
+id é um nome que demos ao parâmetro
+
+para saber mais -> put ou patch
+Sugestão: atualização da transcrição
--- src/app.js
+++ src/app.js
@@ -5,13 +5,19 @@ app.use(express.json()) //middleware -> uma função executando outra função
 const livros = [
     {
         id: 1,
-        título: "O Senhor dos Anéis"
+        titulo: "O Senhor dos Anéis"
     },
     {
         id: 2,
-        título: "O Hobbit"
+        titulo: "O Hobbit"
     }
 ];
+
+function buscaLivros(id){
+    return livros.findIndex(livro => {
+        return livro.id ===Number(id)
+    })
+}
 
 //rotas
 app.get("/", (req, res)=>{
@@ -22,9 +28,20 @@ app.get('/livros', (req, res) => {
     res.status(200).json(livros)
 })
 
+app.get('/livros/:id', (req, res) =>{
+    const index = buscaLivros(req.params.id)
+    res.status(200).json(livros[index]);
+})
+
 app.post('/livros', (req, res)=>{
 livros.push(req.body)
 res.status(201).send("Livro cadastrado com sucesso")
+})
+
+app.put('/livros/:id', (req, res) =>{
+    const index = buscaLivros(req.params.id)
+    livros[index].titulo = req.body.titulo;
+    res.status(200).json(livros)
 })
 
 export default app;
